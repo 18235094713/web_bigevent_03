f4790cad9d3f82ab9d474b0581d858258c705faf
haolijun <2627468062@qq.com>
2020-10-12 21:24:06 +0800

    注册登录功能完成

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,8 @@
+// 开发环境服务器地址
+var baseURL = 'http://ajax.frontend.itheima.net';
+// 每次调用$.get()或$.post()或$.ajax()的时候
+// 会先调用ajaxPrefilter这个函数
+// 在这个函数中 可以拿到我们给ajax提供的配置对象
+$.ajaxPrefilter(function(options) {
+    options.url = baseURL + options.url;
+})
